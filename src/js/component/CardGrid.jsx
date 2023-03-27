0b543ff246817efba963134bd5047523e11fb9a4
Crhistian Alexander Garcia Urbano <garcia.crhistian@correounivalle.edu.co>
2023-03-27 19:42:50 +0000

    first commit

---
+++ src/js/component/CardGrid.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+const CardGrid = () => {
+  return (
+    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
+      <div className="col">
+        <div className="card">
+          <img
+            src="https://www.lambdatest.com/resources/images/news24.gif"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img
+            src="https://www.lambdatest.com/resources/images/news24.gif"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img
+            src="https://www.lambdatest.com/resources/images/news24.gif"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col">
+        <div className="card">
+          <img
+            src="https://www.lambdatest.com/resources/images/news24.gif"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardGrid;
