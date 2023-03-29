88f04ac5b0717aaf87a2d55d5f1b9f5b489014ed
Crhistian Alexander Garcia Urbano <garcia.crhistian@correounivalle.edu.co>
2023-03-29 18:47:38 +0000

    <Card/>

---
+++ src/js/component/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="col">
+      <div className="card">
+        <img
+          src="https://www.lambdatest.com/resources/images/news24.gif"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/js/component/CardGrid.jsx
+++ src/js/component/CardGrid.jsx
@@ -1,76 +1,12 @@
 import React from "react";
+import Card from "./Card.jsx";
 
+let numCards = [0,0,0,0];
+let num = 4;
 const CardGrid = () => {
   return (
     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-      <div className="col">
-        <div className="card">
-          <img
-            src="https://www.lambdatest.com/resources/images/news24.gif"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="col">
-        <div className="card">
-          <img
-            src="https://www.lambdatest.com/resources/images/news24.gif"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="col">
-        <div className="card">
-          <img
-            src="https://www.lambdatest.com/resources/images/news24.gif"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="col">
-        <div className="card">
-          <img
-            src="https://www.lambdatest.com/resources/images/news24.gif"
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-          </div>
-        </div>
-      </div>
+      {numCards.map((value, index) => index < num && <Card key={index}/>)}
     </div>
   );
 };
